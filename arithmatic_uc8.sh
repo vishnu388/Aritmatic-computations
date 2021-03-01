66241f689d44e2511d4214a8d40ae0c29a00e7e0
vishnu <vishnu388@gmail.com>
2021-03-01 22:01:38 +0530

    Added: to commit file uc8

---
+++ arithmatic_uc8.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+echo "enter three inputs:"
+read a b c
+result1=$(($a+$b*$c))
+result2=$(($a*$b+$c))
+result3=$(($c+$a/$b))
+result4=$(($a%$b+$c))
+
+declare -A dictionary
+
+   dictionary[uc2]="$result1"
+	dictionary[uc3]="$result2"
+	dictionary[uc4]="$result3"
+	dictionary[uc5]="$result4"
+
+declare -a Array
+	for ((count=0; count<=${#dictionary[*]}; count++))
+		do
+	Array[count]=${dictionary[uc$((count+1))]}
+		done
+			echo "resulted-Array:" ${Array[@]}
+
+length="${#Array[@]}"
+for ((i=0; i<$length; i++))
+do
+   for ((j=$i; j<$length; j++))
+do
+if [[ ${Array[j]} -gt ${Array[i]} ]]
+then
+   temp=${Array[i]}
+   Array[i]=${Array[j]}
+   Array[j]=$temp
+fi
+done
+done
+echo "sorted-Array(descending order):" ${Array[@]}
